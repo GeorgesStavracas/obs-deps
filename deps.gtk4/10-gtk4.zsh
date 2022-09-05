deadb55e65e757aa6c40457c000f9c18a7793812
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2022-09-05 20:09:05 -0300

    macos: Add GTK4 and libraries build

---
+++ deps.gtk4/10-gtk4.zsh
@@ -0,0 +1,109 @@
+autoload -Uz log_debug log_error log_info log_status log_output
+
+## Dependency Information
+local name='gtk4'
+local version='4.8.0'
+local url='https://gitlab.gnome.org/GNOME/gtk.git'
+local hash="9cc1dcf2a4739d55460675903c595f68478e0811"
+
+## Dependency Overrides
+local -i shared_libs=1
+
+## Build Steps
+setup() {
+  log_info "Setup (%F{3}${target}%f)"
+  setup_dep ${url} ${hash}
+}
+
+clean() {
+  cd "${dir}"
+
+  if [[ ${clean_build} -gt 0 && -f "build_${arch}/build.ninja" ]] {
+    log_info "Clean build directory (%F{3}${target}%f)"
+
+    rm -rf "build_${arch}"
+  }
+}
+
+config() {
+  autoload -Uz mkcd progress
+
+  local build_type
+
+  case ${config} {
+    Debug) build_type='debug' ;;
+    RelWithDebInfo) build_type='debugoptimized' ;;
+    Release) build_type='release' ;;
+    MinSizeRel) build_type='minsize' ;;
+  }
+
+  if (( shared_libs )) {
+    args+=(--default-library both)
+  } else {
+    args+=(--default-library static)
+  }
+
+  case "${target}" {
+    macos-universal)
+      autoload -Uz universal_config && universal_config
+      return
+      ;;
+  }
+
+  log_info "Config (%F{3}${target}%f)"
+  cd "${dir}"
+
+  args+=(
+    --buildtype "${build_type}"
+    --prefix "${target_config[output_dir]}"
+    -Dbuild-tests=false
+    -Dbuild-examples=false
+    -Dcpp_std=c++11
+    -Ddemos=false
+    -Dintrospection=disabled
+    -Dmacos-backend=true
+    -Dmedia-gstreamer=disabled
+    -Dvulkan=disabled
+    -Dx11-backend=false
+    -Dcairo:tests=disabled
+    -Dgdk-pixbuf:tests=false
+    -Dglib:tests=false
+    -Dgraphene:tests=false
+    -Dgtk:werror=true
+    -Dharfbuzz:tests=disabled
+    -Dharfbuzz:docs=disabled
+    -Dpixman:tests=disabled
+    -Dpkg_config_path="${target_config[output_dir]}/lib/pkgconfig"
+  )
+
+  log_debug "Meson configure options: ${args}"
+  PKG_CONFIG_LIBDIR="${target_config[output_dir]}/lib/pkgconfig" \
+  meson setup "build_${arch}" ${args}
+}
+
+build() {
+  autoload -Uz mkcd progress
+
+  case ${target} {
+    macos-universal)
+      autoload -Uz universal_build && universal_build
+      return
+      ;;
+  }
+
+  log_info "Build (%F{3}${target}%f)"
+  cd "${dir}"
+
+  log_debug "Running meson compile -C build_${arch}"
+  meson compile -C "build_${arch}"
+}
+
+install() {
+  autoload -Uz progress
+
+  log_info "Install (%F{3}${target}%f)"
+
+  cd "${dir}"
+
+  meson install -C "build_${arch}"
+}
